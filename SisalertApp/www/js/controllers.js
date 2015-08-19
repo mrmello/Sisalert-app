78e388ca0605f031a6a4369ba77bcdb8ba2b08f1
Marcos <mrmello92@hotmail.com>
2015-08-18 21:31:01 -0300

    Weather module first version

---
+++ SisalertApp/www/js/controllers.js
@@ -0,0 +1,86 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('WeatherCtrl', function ($scope, weatherService) {
+    $scope.weather = weatherService.getWeather();
+})
+
+.factory('weatherService', function($http) {
+    return { 
+      getWeather: function() {
+        var weather = { temp: {}, clouds: null };
+        $http.jsonp('http://api.openweathermap.org/data/2.5/weather?q=Brasilia,br&units=metric&callback=JSON_CALLBACK').success(function(data) {
+            if (data) {
+                if (data.main) {
+                    weather.temp.current = data.main.temp;
+                    weather.temp.min = data.main.temp_min;
+                    weather.temp.max = data.main.temp_max;
+                }
+                weather.clouds = data.clouds ? data.clouds.all : undefined;
+            }
+        });
+
+        return weather;
+      }
+    }; 
+})
+
+.filter('temp', function($filter) {
+    return function(input, precision) {
+        if (!precision) {
+            precision = 1;
+        }
+        var numberFilter = $filter('number');
+        return numberFilter(input, precision) + '\u00B0C';
+    };
+})
+
+.directive('weatherIcon', function() {
+    return {
+        restrict: 'E', replace: true,
+        scope: {
+            cloudiness: '@'
+        },
+        controller: function($scope) {
+            $scope.imgurl = function() {
+                var baseUrl = 'https://ssl.gstatic.com/onebox/weather/128/';
+                if ($scope.cloudiness < 20) {
+                    return baseUrl + 'sunny.png';
+                } else if ($scope.cloudiness < 90) {
+                   return baseUrl + 'partly_cloudy.png';
+                } else {
+                    return baseUrl + 'cloudy.png';
+                }
+            };
+        },
+        template: '<div style="float:left"><img ng-src="{{ imgurl() }}"></div>'
+    };
+});
---
+++ SisalertApp/www/templates/script.js
@@ -0,0 +1,91 @@
+// Code goes here
+
+var app = angular.module('app', []);
+
+
+app.controller('MainController', function($scope) {
+
+  $scope.text = "Hello Angular!";
+
+});
+
+app.controller('StoreController', function($scope) {
+  
+  var gem = {
+  name: 'Diamond',
+  price: 120,
+  description: 'Hard'
+};
+
+    $scope.product = gem;
+  }
+);
+
+app.controller('PersonController', function($scope) {
+
+  var product = {
+    firstName: "Ajay",
+    lastName: "Sattikar",
+    imageSrc: "Images/scott_allen_2.jpg",
+  };
+
+  $scope.text = "Hello Angular!";
+  $scope.product = product;
+
+});
+
+app.factory('weatherService', function($http) {
+    return { 
+      getWeather: function() {
+        var weather = { temp: {}, clouds: null };
+        $http.jsonp('http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&callback=JSON_CALLBACK').success(function(data) {
+            if (data) {
+                if (data.main) {
+                    weather.temp.current = data.main.temp;
+                    weather.temp.min = data.main.temp_min;
+                    weather.temp.max = data.main.temp_max;
+                }
+                weather.clouds = data.clouds ? data.clouds.all : undefined;
+            }
+        });
+
+        return weather;
+      }
+    }; 
+});
+
+app.filter('temp', function($filter) {
+    return function(input, precision) {
+        if (!precision) {
+            precision = 1;
+        }
+        var numberFilter = $filter('number');
+        return numberFilter(input, precision) + '\u00B0C';
+    };
+});
+
+app.controller('WeatherCtrl', function ($scope, weatherService) {
+    $scope.weather = weatherService.getWeather();
+});
+
+app.directive('weatherIcon', function() {
+    return {
+        restrict: 'E', replace: true,
+        scope: {
+            cloudiness: '@'
+        },
+        controller: function($scope) {
+            $scope.imgurl = function() {
+                var baseUrl = 'https://ssl.gstatic.com/onebox/weather/128/';
+                if ($scope.cloudiness < 20) {
+                    return baseUrl + 'sunny.png';
+                } else if ($scope.cloudiness < 90) {
+                   return baseUrl + 'partly_cloudy.png';
+                } else {
+                    return baseUrl + 'cloudy.png';
+                }
+            };
+        },
+        template: '<div style="float:left"><img ng-src="{{ imgurl() }}"></div>'
+    };
+});
