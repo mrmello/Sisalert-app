b647273eef87aac0445cefd1ee44547d825ece75
Marcos <mrmello92@hotmail.com>
2015-08-23 15:59:51 -0300

    Improvments weather module

--- SisalertApp/www/js/controllers.js
+++ SisalertApp/www/js/controllers.js
@@ -83,4 +83,33 @@ angular.module('starter.controllers', [])
         },
         template: '<div style="float:left"><img ng-src="{{ imgurl() }}"></div>'
     };
+})
+
+.factory('openweather', function($http) {
+  var runRequest = function(city) {
+    return $http({
+      method: 'JSONP',
+      url: 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+ city + '&lang=pt&mode=json&units=metric&cnt=4&callback=JSON_CALLBACK'
+    });
+  };
+  return {
+    event: function(city) {
+      return runRequest(city);
+    }
+  };
+})
+
+.controller('WeatherForecastCtrl', function($scope, $timeout, openweather){
+  var timeout;    
+  $scope.$watch('city', function(newCity) {
+    if(newCity) {
+      if(timeout) $timeout.cancel(timeout);
+      timeout = $timeout(function() {
+        openweather.event(newCity).success(function(data, status) { 
+          $scope.loc = data;
+          $scope.forecast = data.list;
+        });
+      }, 1000);
+    }
+  });
 });
