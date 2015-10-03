3ad3f476d08d2bd7b6924794442f7adca69f46e6
Marcos <mrmello92@hotmail.com>
2015-10-03 20:24:16 -0300

    maps api and filters

--- SisalertApp/www/js/controllers.js
+++ SisalertApp/www/js/controllers.js
@@ -112,4 +112,117 @@ angular.module('starter.controllers', [])
       }, 1000);
     }
   });
+})
+
+.controller('MapCtrl', function ($scope) {
+
+	var cities = [
+    {
+        city : 'Toronto',
+        region: 'Nordeste',
+        url: 'https://en.wikipedia.org/wiki/Toronto',
+        lat : 43.7000,
+        long : -79.4000
+    },
+    {
+        city : 'New York',
+        region: 'Nordeste',
+        url: 'https://en.wikipedia.org/wiki/Toronto',
+        lat : 40.6700,
+        long : -73.9400
+    },
+    {
+        city : 'Chicago',
+        region: 'Nordeste',
+        url: 'https://en.wikipedia.org/wiki/Toronto',
+        lat : 41.8819,
+        long : -87.6278
+    },
+    {
+        city : 'Los Angeles',
+        region: 'Leste',
+        url: 'https://en.wikipedia.org/wiki/Toronto',
+        lat : 34.0500,
+        long : -118.2500
+    },
+    {
+        city : 'Las Vegas',
+        region: 'Leste',
+        url: 'https://en.wikipedia.org/wiki/Toronto',
+        lat : 36.0800,
+        long : -115.1522
+    }
+];
+
+    var mapOptions = {
+        zoom: 4,
+        center: new google.maps.LatLng(40.0000, -98.0000),
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
+   
+  	$scope.$watch('city', function(newCity) {
+	    if(newCity) {
+	      filterMarkers(newCity);
+	    } else{
+	    	 filterMarkers('');
+	    }
+	  });
+
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    $scope.markers = [];
+    
+    var infoWindow = new google.maps.InfoWindow();
+    
+    var createMarker = function (info){
+        
+        var marker = new google.maps.Marker({
+            map: $scope.map,
+            position: new google.maps.LatLng(info.lat, info.long),
+            title: info.city,
+            region: info.region
+        });
+        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+        
+        google.maps.event.addListener(marker, 'click', function(){
+
+        	//var win = window.open(info.url, '_blank');
+  			//win.focus();
+            infoWindow.setContent('<h2>' + marker.title + '</h2>' + info.region);
+            infoWindow.open($scope.map, marker);
+        });
+        
+        $scope.markers.push(marker);
+        
+    }  
+    
+    for (i = 0; i < cities.length; i++){
+        createMarker(cities[i]);
+    }
+
+    $scope.openInfoWindow = function(e, selectedMarker){
+        e.preventDefault();
+        google.maps.event.trigger(selectedMarker, 'click');
+    }
+
+    var bounds = new google.maps.LatLngBounds();
+	for(i=0;i<$scope.markers.length;i++) {
+		bounds.extend($scope.markers[i].getPosition());
+	}
+
+	$scope.map.fitBounds(bounds);
+
+    filterMarkers = function (filter) {
+    for (i = 0; i < $scope.markers.length; i++) {
+        marker = $scope.markers[i];
+        var city = marker.title;
+        if (marker.region == filter || ( city.toLowerCase().match(filter.toLowerCase()))|| filter.length === 0) {
+            marker.setVisible(true);
+        }
+        else {
+            marker.setVisible(false);
+        }
+    }
+}
+
 });
